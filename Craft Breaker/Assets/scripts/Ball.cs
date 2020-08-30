f60007d6efb508bd4544d5b2404dc797f907fb69
Cory Leclair <cory.leclair@gmail.com>
2020-08-30 13:25:44 -0400

    Aug 30 - Adding SFX

--- Craft Breaker/Assets/scripts/Ball.cs
+++ Craft Breaker/Assets/scripts/Ball.cs
@@ -10,16 +10,21 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 10f;
+    [SerializeField] AudioClip[] ballSounds;
 
     //State
 
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+    //Cached Refs
+
+    AudioSource myAudioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,24 +35,41 @@ public class Ball : MonoBehaviour
             LockBallToPaddle();
             LaunchOnClick();
         }
-        
+
     }
 
     private void LaunchOnClick()
     {
-        
+
         if (Input.GetMouseButtonDown(0))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
             hasStarted = true;
         }
-        
+
     }
 
     private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        string hasCollided = collision.gameObject.name;
+        
+        if (hasStarted && hasCollided == "Paddle")
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+             myAudioSource.PlayOneShot(clip);
+
+              
+        }
         
     }
+
+  
 }
--- Craft Breaker/Assets/scripts/Block.cs
+++ Craft Breaker/Assets/scripts/Block.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip destroySound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position);
         Destroy(gameObject);
+
+        
     }
 }
