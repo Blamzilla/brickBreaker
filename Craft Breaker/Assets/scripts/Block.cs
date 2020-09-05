cadb0ab0c8458df20b07e25731a37be8b8e653ea
Cory Leclair <cory.leclair@gmail.com>
2020-09-05 14:52:38 -0400

    added damage affordance and unbreakable assets

--- Craft Breaker/Assets/scripts/Block.cs
+++ Craft Breaker/Assets/scripts/Block.cs
@@ -10,13 +10,15 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip destroySound;
     [SerializeField] GameObject blockSparkleVFX;
-
+    [SerializeField] int maxHits=2;
+    [SerializeField] Sprite[] hitSprites;
     //Cached ref to Level
 
     Level level;
-    
-    
-   
+
+
+    // State Variables
+    [SerializeField] int timesHit; // TODO only serialized for debugging
 
     private void Start()
     {
@@ -29,19 +31,39 @@ public class Block : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        FindObjectOfType<GameSession>().AddToScore();
-        AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position);
-        Destroy(gameObject);
-        level.brickBroken();
-        TriggerSparklesVFX();
-        
-        
-
-       
-        
-
+        BreakBlock();
 
     }
+
+    private void BreakBlock()
+    {
+        AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position);
+
+        if (tag == "Breakable")
+        {
+            timesHit++;
+            if (maxHits == timesHit)
+            {
+                
+                FindObjectOfType<GameSession>().AddToScore();
+                Destroy(gameObject);
+                level.brickBroken();
+                TriggerSparklesVFX();
+            }
+            else
+            {
+                ShowNextHitSprite();
+            }
+            
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
     private void TriggerSparklesVFX()
     {
         GameObject sparkles = Instantiate(blockSparkleVFX, transform.position, transform.rotation);
